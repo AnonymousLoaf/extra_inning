f2653b6670d5daec8c5a3e65ec201346775ccaee
Noah Seward <10894734@uvu.edu>
2024-07-23 01:05:55 -0600

    Working on converting to excel

--- get_nominations.py
+++ get_nominations.py
@@ -1,7 +1,8 @@
+import pandas as pd
 from player import Player
-import csv
 
-class CSVData:
+
+class ExcelData:
     def __init__(self, file):
         self.file = file
         self.players = []
@@ -9,10 +10,9 @@ class CSVData:
 
     def load_players(self):
         try:
-            with open(self.file, 'r', encoding='utf-8-sig') as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    self.players.append(Player(**row))
+            df = pd.read_excel(self.file)
+            for _, row in df.iterrows():
+                self.players.append(Player(**row.to_dict()))
         except FileNotFoundError:
             print(f"File '{self.file}' not found.")
         except Exception as e:
@@ -21,18 +21,17 @@ class CSVData:
     def update_player_attr_names(self):
         for player in self.players:
             for old_name, new_name in self.attr_mapping.items():
-                if hasattr(player, old_name):
+                if hasattr(player, old_name.strip()):
                     setattr(player, new_name, getattr(player, old_name))
                     delattr(player, old_name)
         self.attr_names = list(self.attr_mapping.values())
 
-
     def get(self, name):
         for player in self.players:
-            if player.PlayerName == name:
+            if player.PlayerFirstName + " " + player.PlayerLastName == name:
                 return player
         return None
-    
+
     attr_mapping = {
         "Name of player (First)": "PlayerFirstName",
         "Name of player (Last)": "PlayerLastName",
@@ -42,48 +41,40 @@ class CSVData:
         "Club Coach Email": "ClubCoachEmail",
         "Club Coach Phone Number": "ClubCoachPhone",
         "Club Team Name": "ClubTeamName",
-        "What organization does this player play in?": "PlayerOrganization",
         "Organization Point of Contact Name": "OrganizationContactName",
         "Organization Point of Contact Phone Number": "OrganizationContactPhone",
         "Game Changer Team Name": "GameChangerTeamName",
         "Primary Position of Player": "PlayerPosition",
         "High School Name": "HighSchoolName",
-        "What is this player's BA average over the last 12 months? [required]" : "PlayerBA",
-        "What is this player's OBP average over the last 12 months? [required]" : "PlayerOBP",
-        "What is this player's OPS average over the last 12 months? [required]" : "PlayerOPS",
-        "What is this player's QAB% average over the last 12 months? [required]" : "PlayerQAB",
-        "What is this player's fielding % average over the last 12 months? [required]": "FieldingPerc",
-        "What is this player's arm velo?" : "ArmVelo",
-        "What is this player's TCs % average over the last 12 months? [required]" : "TotalChances",
-        "Average for Assists (total / innings played over last 12 months?" : "Assists",
-        "Average POs (total / innings played over the last 12 months?" : "Putouts",
-        "What is this players ERA over the last 12 months?" : "PlayerERA",
-        "What is this players WHIP over the last 12 months?" : "PlayerWHIP",
-        "What is this players Ks over the last 12 months?" : "PlayerKs",
-        "What is this players BB (walks) over the last 12 months?" : "PlayerBB",
-        "What is this players IP (innings pitched) over the last 12 months?" : "PlayerIP",
-        "What is this players BAA over the last 12 months?" : "PlayerBAA",
-        "What is this pitchers fastball speed?" : "PlayerFastballSpeed",
-        "What is this pitchers change-up speed?" : "PlayerChangeUpSpeed",
-        "What is this player's pop-time?" : "PlayerPopTime",
-        "Where do you think this player should be ranked nationally?" : "PlayerRankingEstimate",
-        "Why should this player be ranked here?" : "PlayerRankingReason",
+        "Batting Avg.": "PlayerBA",
+        "OBP": "PlayerOBP",
+        "OPS": "PlayerOPS",
+        "QAB%": "PlayerQAB",
+        "Fielding %": "FieldingPerc",
+        "TCs": "TotalChances",
+        "Assists": "Assist",
+        "Pos": "Putouts",
+        "Arm Velo": "ArmVelo",
+        "ERA": "PlayerERA",
+        "WHIP": "PlayerWHIP",
+        "Ks": "PlayerKs",
+        "BB": "PlayerBB",
+        "IP": "PlayerIP",
+        "BAA": "PlayerBAA",
+        "P's fastball speed?": "PlayerFastballSpeed",
+        "Ps change-up speed?": "PlayerChangeUpSpeed",
+        " pop-time?": "PlayerPopTime",
+        " arm velo?": "ArmVelo2",
+        "Coach: where to rank?": "PlayerRankingEstimate",
         "Share this players individual accomplishments (List in bulleted format)": "PlayerAccomplishments",
-        "Events this player participate in over the last 12 months? (list in bulleted format)": "PlayerEvents",
-        "Name at least 3 quality teams this player faced over the last 12 months": "PlayerQualityTeams",
-        "Which Region does this player live in?": "PlayerRegion",
-        "Player Facebook  (Example: Extra Inning Softball)": "PlayerFacebook",
-        "Player Instagram Handle (Example: extrainningsoftball)": "PlayerInstagram",
-        "Player X - Twitter Handle (Example: @ExtraInningSB)": "PlayerTwitter",
-        "Parent(s) or Guardian Name": "ParentName",
-        "Parent(s) or Guardian Email": "ParentEmail",
-        "Parent(s) or Guardian Phone Number": "ParentPhone",
-        "College committed to (if applicable)": "PlayerCollegeCommitted",
-        "Secondary/Other Position(s) of Player: (Mark as N/A if not applicable)": "PlayerSecondaryPosition",
+        "List 3 top events & 3 top opponents": "PlayerEventsandOpponents",
+        "Region": "PlayerRegion",
+        "Instagram": "PlayerInstagram",
+        "Twitter": "PlayerTwitter",
+        "Parent Name": "ParentName",
+        "Parent Email": "ParentEmail",
+        "Parent Phone": "ParentPhone",
         "Player Hometown/State": "PlayerHometown",
-        "Provide a quote from one of the player's coaches (may be published)": "PlayerQuote",
-        "Human Interest Insights": "PlayerHumanInterest",
-        "Recruiting news (if applicable)": "PlayerRecruitingNews",
+        "Coach Quote": "PlayerQuote",
         "Headshot": "PlayerHeadshot",
-        "Action Photo": "PlayerActionPhoto",
     }
--- main.py
+++ main.py
@@ -1,69 +1,105 @@
-import tkinter as tk
-from tkinter import filedialog
-from get_nominations import CSVData
-from compare import players_pitcher_score, players_defensive_score, players_catcher_score, players_batting_score
-import csv
 import os
+import tkinter as tk
+import pandas as pd
+from tkinter import filedialog, messagebox
+from get_nominations import ExcelData
+from compare import (
+    players_pitcher_score,
+    players_defensive_score,
+    players_catcher_score,
+    players_batting_score,
+)
+
 
 class PlayerNominationApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Player Nomination Application")
 
-        self.file_label = tk.Label(self.root, text="Select CSV File:")
+        self.file_label = tk.Label(self.root, text="Select XLSX File:")
         self.file_label.pack()
 
         self.file_entry = tk.Entry(self.root, width=50)
         self.file_entry.pack()
 
-        self.browse_button = tk.Button(self.root, text="Browse", command=self.browse_file)
+        self.browse_button = tk.Button(
+            self.root, text="Browse", command=self.browse_file
+        )
         self.browse_button.pack()
 
         self.run_button = tk.Button(self.root, text="Run", command=self.run_script)
         self.run_button.pack()
 
     def browse_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+        file_path = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
         self.file_entry.delete(0, tk.END)
         self.file_entry.insert(0, file_path)
 
     def run_script(self):
         file = self.file_entry.get()
         if not os.path.isfile(file):
-            tk.messagebox.showerror("Error", f"Cannot find file '{file}'.")
+            messagebox.showerror("Error", f"Cannot find file '{file}'.")
             return
 
-        data = CSVData(file)
+        data = ExcelData(file)
         data.load_players()
         data.update_player_attr_names()
         players = data.players
 
         output_directory = os.path.dirname(file)
 
-        pitchers = [player for player in players if player.PlayerPosition == "Pitcher" or player.PlayerSecondaryPosition == "Pitcher"]
+        find = "Mady Rhone"
+        
+        player = data.get(find)
+        if player is None:
+            messagebox.showerror("Error", f"Cannot find player '{find}'.")
+            return
+
+        pitchers = [
+            player
+            for player in players
+            if player.PlayerPosition == "Pitcher"
+        ]
         pitchers = players_pitcher_score(pitchers)
-        self.save_to_csv(pitchers, os.path.join(output_directory, "Pitchers.csv"), data.attr_names)
+        self.save_to_excel(
+            pitchers, os.path.join(output_directory, "Pitchers.xlsx"), data.attr_names
+        )
 
-        defensive_players = [player for player in players if player.PlayerPosition in ["MIF", "CIF", "Outfielder"] or player.PlayerSecondaryPosition in ["MIF", "CIF", "Outfielder"]]
+        defensive_players = [
+            player
+            for player in players
+            if player.PlayerPosition in ["MIF", "CIF", "Outfielder"]
+        ]
         defensive_players = players_defensive_score(defensive_players)
-        self.save_to_csv(defensive_players, os.path.join(output_directory, "Defense.csv"), data.attr_names)
+        self.save_to_excel(
+            defensive_players,
+            os.path.join(output_directory, "Defense.xlsx"),
+            data.attr_names,
+        )
 
-        catchers = [player for player in players if player.PlayerPosition == "Catcher" or player.PlayerSecondaryPosition == "Catcher"]
+        catchers = [
+            player
+            for player in players
+            if player.PlayerPosition == "Catcher"
+        ]
         catchers = players_catcher_score(catchers)
-        self.save_to_csv(catchers, os.path.join(output_directory, "Catchers.csv"), data.attr_names)
+        self.save_to_excel(
+            catchers, os.path.join(output_directory, "Catchers.xlsx"), data.attr_names
+        )
 
         batters = players
         batters = players_batting_score(batters)
-        self.save_to_csv(batters, os.path.join(output_directory, "Batters.csv"), data.attr_names)
+        self.save_to_excel(
+            batters, os.path.join(output_directory, "Batters.xlsx"), data.attr_names
+        )
 
-        tk.messagebox.showinfo("Success", "Processing complete.")
+        messagebox.showinfo("Success", "Processing complete.")
 
-    def save_to_csv(self, players, file_path, fieldnames):
-        with open(file_path, 'w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for player in players:
-                writer.writerow(player.__dict__)
+    def save_to_excel(self, players, file_path, fieldnames):
+        data = [player.__dict__ for player in players]
+        df = pd.DataFrame(data, columns=fieldnames)
+        df.to_excel(file_path, index=False)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
